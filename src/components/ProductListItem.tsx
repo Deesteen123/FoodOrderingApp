2a030d4b98ab3def9163090e5fb31de143909436
Uriel Ngadeu <ungadeu27@gmail.com>
2024-02-24 21:20:57 -0500

    update 4

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,50 +1,56 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImages =
-  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png';
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 type ProdcutListItemProps = {
     product: Product;
-}
+};
 
-const ProductListItem = ({ product }: ProdcutListItemProps ) => {
-    return (
-      <View style={styles.container}>
+const ProductListItem = ({ product }: ProdcutListItemProps) => {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
         <Image 
-        source={{ uri: product.image || defaultPizzaImages }} 
-        style={styles.image} 
+          source={{ uri: product.image || defaultPizzaImages }} 
+          style={styles.image} 
+          resizeMode="contain"
         />
         
         <Text style={styles.title}>{product.name}</Text>
         <Text style={styles.price}>${product.price}</Text>
-      </View>
-    );
-  };
 
-  export default ProductListItem;
+      </Pressable>
+    </Link>
+  );
+};
+
+export default ProductListItem;
   
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 20,
-    },
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
+  },
   
-    image: {
-      width: '100%',
-      aspectRatio: 1,
-    },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
   
-    title: { 
-      fontSize: 18, 
-      fontWeight: '600', 
-      marginVertical: 10,
-    },
-    price: {
-      color: Colors.light.tint,
-      fontWeight: 'bold',
-    },
-  });
-  
+  title: { 
+    fontSize: 18, 
+    fontWeight: '600', 
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+});
